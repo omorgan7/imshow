1d70635af7d6657042fc160c876a536d0072682f
Owen Morgan <o.morgan@bath.ac.uk>
2018-08-23 18:24:09 +0100

    Updated demo test program.

--- Demo/test.c
+++ Demo/test.c
@@ -1,13 +1,15 @@
 #include "imshow.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
 
 
 int main()
 {  
 
     const int width = 600;
-    const int height = 900;
+    const int height = 800;
     const int bufSize = width * height;
     unsigned char* imageBuffer = (unsigned char*) malloc(bufSize);
 
@@ -17,9 +19,14 @@ int main()
         }
     }
 
-    if (imshow_u8_c1("My Window", imageBuffer, width, height)) {
-        printf("something went wrong\n");
+    for (int i = 0; i < width * height; ++i) {
+        if (imshow_u8_c1("My Window", imageBuffer, width, height)) {
+            break;
+        }
+        imageBuffer[i] = 0;
     }
+
+
 
     free(imageBuffer);
     
